6332d900ecb28bae0705d751c0b718212c6ba9f0
ivanmaleev@inbox.ru <mudr6PHEL9$>
2022-01-10 19:34:58 +0300

    2. Асинхронное взаимодействие [#458497]

---
+++ kafka-client/src/main/java/ru/job4j/kafkaclient/KafkaClientApplication.java
@@ -0,0 +1,13 @@
+package ru.job4j.kafkaclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaClientApplication.class, args);
+    }
+
+}
